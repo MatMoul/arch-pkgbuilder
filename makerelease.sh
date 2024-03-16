0b76b278870a419a0c53d869ef58a824d653e432
MatMoul <matmoul@gmail.com>
2024-03-16 22:56:56 +0100

    Add makerelease script

---
+++ makerelease.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+declare -r VERSION=${1}
+declare -r MESSAGE=${2}
+declare -r TAGBRANCH=main
+declare CURRENTBRANCH=""
+
+showHelp() {
+	echo git-release version
+}
+
+if [ "${VERSION}" == "" ]; then
+	showHelp
+	echo ""
+	echo "no version provided!"
+	exit 1
+fi
+
+ssh -T git@github.com
+if [ ! "${?}" = "1" ]; then
+  echo "No Github ssh key loaded exiting..."
+  exit 1
+fi
+
+
+CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ ! "${CURRENTBRANCH}" == "dev" ]; then
+	echo "You are not in dev branch!"
+	echo "Use dev branch to make a release!"
+	exit 1
+fi
+
+git checkout "${TAGBRANCH}"
+git merge "${CURRENTBRANCH}"
+git push
+git tag -a "${VERSION}" -m "${MESSAGE}"
+git push --tags
+git checkout "${CURRENTBRANCH}"
